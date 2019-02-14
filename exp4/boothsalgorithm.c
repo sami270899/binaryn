2e24c3e3fc40f64acd1fbe8039ba3f4d11c962b5
sami270899 <gawadesam08@gmail.com>
2019-02-14 10:46:51 +0530

    new changes to booths algorithm

--- exp4/boothsalgorithm.c
+++ exp4/boothsalgorithm.c
@@ -1,48 +1,38 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-int convertBinaryToDecimal(int n);
 int convertDecimalToBinary(int n);
 int main()
 {
     int p,n;
-    while(1){
-    printf("\nPress 1 to convert binary to decimal\n");
-    printf("\nPress 2 to convert decimal to binary\n");
-    scanf("%d",&p);
-    switch(p)
+    int i,j;
+    int a[20],q[20];
+    int m[20];
+    int multiplicand,multiplier;
+    printf("Enter a decimal number(multiplicand): ");
+    scanf("%d", &n);
+    multiplicand=convertDecimalToBinary(n);
+
+    for(j=0;j<18;j++)
     {
-        case 1: printf("Enter a binary number: ");
-                scanf("%d", &n);
-                printf("%d in binary = %d in decimal", n, convertBinaryToDecimal(n));
-                break;
-        case 2: printf("Enter a decimal number: ");
-                scanf("%d", &n);
-                printf("%d in decimal = %d in binary", n, convertDecimalToBinary(n));
-                break;
+        scanf("%d",&mn[20]);
     }
-}
-	printf("print");
+    printf("\nThe mutiplicand is:%d",multiplicand);
+    printf("\nEnter a decimal number(multiplier): ");
+    scanf("%d", &p);
+    multiplier=convertDecimalToBinary(p);
+    printf("\nThe mutipliplier is:%d",multiplier);
+    for(j=0;j<18;j++)
+    {
+        scanf("%d",&m[20]);
+    }
     return 0;
 }
 
-int convertBinaryToDecimal(int n)
-{
-    int decimalNumber = 0, i = 0, remainder;
-    while (n!=0)
-    {
-        remainder = n%10;
-        n /= 10;
-        decimalNumber += remainder*pow(2,i);
-        ++i;
-    }
-    return decimalNumber;
-}
 int convertDecimalToBinary(int n)
 {
     int binaryNumber = 0;
     int remainder, i = 1;
-
     while (n!=0)
     {
         remainder = n%2;
@@ -51,4 +41,5 @@ int convertDecimalToBinary(int n)
         i *= 10;
     }
     return binaryNumber;
+
 }
